44a099dea82087af068a518cb45b7fbdbc6aa10d
myturn82 <96092889+myturn82@users.noreply.github.com>
2025-05-07 23:44:44 +0900

    no message

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://uzjdnbdkdlkbshmvsnxb.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6amRuYmRrZGxrYnNobXZzbnhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4MDkwOTksImV4cCI6MjA2MTM4NTA5OX0.nsjrSI_AZe2BkwmG5RTZyDE7FF1JrDoQGK3k7IN82uQ';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
